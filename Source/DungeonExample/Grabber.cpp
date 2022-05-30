1740e51ad789427fc2506d1e02bef9a7e26ce507
Katsanakis Georgios <geo.elgeo@gmail.com>
2022-05-30 12:14:07 +0200

    refactored grabber

--- Source/DungeonExample/Grabber.cpp
+++ Source/DungeonExample/Grabber.cpp
@@ -49,17 +49,8 @@ void UGrabber::Grab() {
 		return;
 	}
 
-
-	FVector Start = GetComponentLocation();
-	FVector End = Start + GetForwardVector() * MaxGrabDistance;
-
-	DrawDebugLine(GetWorld(), Start, End, FColor::Red);
-	//DrawDebugSphere(GetWorld(), End, 10, 10, FColor::Blue, false, 5);
-
-	FCollisionShape Sphere = FCollisionShape::MakeSphere(GrabRadius);
 	FHitResult HitResult;
-	bool HasHit = GetWorld()->SweepSingleByChannel(HitResult, Start, End, FQuat::Identity, ECC_GameTraceChannel2,
-		Sphere);
+	bool HasHit = GetGrabbableInReach(HitResult);	
 
 	if (HasHit) {
 		UPrimitiveComponent* HitComponent = HitResult.GetComponent();
@@ -90,4 +81,17 @@ UPhysicsHandleComponent* UGrabber::GetPhysicsHandle() const {
 		UE_LOG(LogTemp, Error, TEXT("Grabber requires a UPhysicsHandleComponent"));
 	}
 	return Result;
+}
+
+bool UGrabber::GetGrabbableInReach(FHitResult& OutHitResult)const {
+	FVector Start = GetComponentLocation();
+	FVector End = Start + GetForwardVector() * MaxGrabDistance;
+
+	DrawDebugLine(GetWorld(), Start, End, FColor::Red);
+	//DrawDebugSphere(GetWorld(), End, 10, 10, FColor::Blue, false, 5);
+
+	FCollisionShape Sphere = FCollisionShape::MakeSphere(GrabRadius);
+	
+	return  GetWorld()->SweepSingleByChannel(OutHitResult, Start, End, FQuat::Identity, ECC_GameTraceChannel2,
+		Sphere);
 }
--- Source/DungeonExample/Grabber.h
+++ Source/DungeonExample/Grabber.h
@@ -44,4 +44,6 @@ private:
 		float HoldDistance = 200;
 
 	UPhysicsHandleComponent* GetPhysicsHandle() const;
+
+	bool GetGrabbableInReach(FHitResult& OutHitResult) const;
 };
