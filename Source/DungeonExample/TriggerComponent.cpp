b2147ba6c6ae632f15ac4eabb12df8b495296aa8
Katsanakis Georgios <geo.elgeo@gmail.com>
2022-05-30 21:04:22 +0200

    added constructor to enable ticking

--- Source/DungeonExample/TriggerComponent.cpp
+++ Source/DungeonExample/TriggerComponent.cpp
@@ -4,9 +4,18 @@
 #include "TriggerComponent.h"
 
 // Called when the game starts
+UTriggerComponent::UTriggerComponent() {
+
+	// enables ticking
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+
 void UTriggerComponent::BeginPlay()
 {
 	Super::BeginPlay();
+}
 
-
+void  UTriggerComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) {
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 }
--- Source/DungeonExample/TriggerComponent.h
+++ Source/DungeonExample/TriggerComponent.h
@@ -14,7 +14,14 @@ class DUNGEONEXAMPLE_API UTriggerComponent : public UBoxComponent
 {
 	GENERATED_BODY()
 
+public:
+	UTriggerComponent();
+
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 };
