83702cb215e080b538c6a505518b913309b2ccb4
Katsanakis Georgios <geo.elgeo@gmail.com>
2022-05-31 09:24:23 +0200

    small adjusting

--- Source/DungeonExample/TriggerComponent.cpp
+++ Source/DungeonExample/TriggerComponent.cpp
@@ -19,13 +19,33 @@ void UTriggerComponent::BeginPlay()
 void  UTriggerComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
+	AActor* Actor = GetAcceptableActor();
+	if (Actor != nullptr) {
+		UE_LOG(LogTemp, Display, TEXT("Unlocking"));
+	}
+	else {
+		UE_LOG(LogTemp, Display, TEXT("Relocking"));
+	}
+}
+
+AActor* UTriggerComponent::GetAcceptableActor() const {
+	
 	TArray<AActor*> Actors;
 	GetOverlappingActors(Actors);
 
-	if (Actors.Num()>0){
-		for (int num = 0; num < Actors.Num(); num++) {
+	if (Actors.Num() > 0) {
+		/*for (int num = 0; num < Actors.Num(); num++) {
 			FString ActorName = Actors[num]->GetActorNameOrLabel();
 			UE_LOG(LogTemp, Display, TEXT("Overlapping: %s"), *ActorName);
+		}*/
+
+		// Alternative loop
+		for (AActor* Actor : Actors) {
+			if (Actor->ActorHasTag(AcceptableActorTag)) {
+				return Actor;
+			}
 		}
 	}
+
+	return nullptr;
 }
--- Source/DungeonExample/TriggerComponent.h
+++ Source/DungeonExample/TriggerComponent.h
@@ -24,4 +24,10 @@ protected:
 public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+private:
+	UPROPERTY(EditAnywhere)
+		FName AcceptableActorTag;
+
+	AActor* GetAcceptableActor() const;
 };
